b435a592e8782425ce0b0fc38baac263d9872191
YahyaYacoob <Yahya@Yahyas-Air.fritz.box>
2025-01-19 19:28:21 +0100

    Initial commit

--- types/next-env.d.ts
+++ types/next-env.d.ts
@@ -1,1 +1,7 @@
- 
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/navigation" />
+
+declare module '@sentry/nextjs'
+declare module '@paypal/react-paypal-js'
+declare module 'socket.io-client' 
